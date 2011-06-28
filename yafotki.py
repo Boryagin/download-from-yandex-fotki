47f958f196db28cf025b7891636a583515467ba9
Sergey Ponomarev <ponomarevsa@yandex.ru>
2011-06-28 17:00:44 +0600

    Encrypt a couple of login and password received RSA-key.

--- yafotki.py
+++ yafotki.py
@@ -42,6 +42,9 @@ class sync_yafotki():
         # Request the server to the public RSA-key.
         self._request_key();
 
+        # Encrypt a couple of login and password received RSA-key.
+        self._crypt()
+
     def _request_key(self):
         """
         Request the server to the public RSA-key and request_id.
@@ -52,6 +55,39 @@ class sync_yafotki():
         self._key = soup.key.string
         self._request_id = soup.request_id.string
         return True
+
+    def _crypt(self):
+        """
+        Encrypt a couple of login and password received RSA-key.
+        see for detail: http://habrahabr.ru/blogs/firefox/83710/
+        """
+
+        NSTR, ESTR = self._key.split("#")
+        credentials = '<credentials login="%s" password="%s"/>' % (self.username, self.password)
+        DATA_ARR = [ord(x) for x in credentials]
+        N, E, STEP_SIZE = int(NSTR, 16), int(ESTR, 16), len(NSTR) / 2 - 1
+
+        prev_crypted = [0] * STEP_SIZE
+
+        hex_out = ""
+        for i in range(0, (len(DATA_ARR) - 1) / STEP_SIZE + 1):
+            tmp = DATA_ARR[i * STEP_SIZE:(i + 1) * STEP_SIZE]
+            tmp = [tmp[i] ^ prev_crypted[i] for i in range(0, len(tmp))]
+            tmp.reverse()
+            plain = 0
+            for x in range(0, len(tmp)): plain += tmp[x] * pow(256, x, N)
+            hex_result = "%x" % pow(plain, E, N)
+            hex_result = "".join(['0'] * ( len(NSTR) - len(hex_result))) + hex_result
+
+            for x in range(0, min(len(hex_result), len(prev_crypted) * 2), 2):
+                prev_crypted[x / 2] = int(hex_result[x:x + 2], 16)
+
+            hex_out += ("0" if len(tmp) < 16 else "") + ("%x" % len(tmp)) + "00" # current size
+            ks = len(NSTR) / 2
+            hex_out += ("0" if ks < 16 else "") + ("%x" % ks) + "00" # key size
+            hex_out += hex_result
+
+        self._rsa_key = hex_out.decode("hex").encode("base64").replace("\n", "")
 
 if __name__ == '__main__':
     y = sync_yafotki("yafotkimytest123", "g2h9h4")
